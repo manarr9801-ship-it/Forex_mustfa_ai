dbeda2b741d5d405a2ba761554d87d934d4a22f4
manarr9801-ship-it <manarr9801@gmail.com>
2026-07-14 16:04:33 +0300

    Update risk_manager.py

--- risk_manager.py
+++ risk_manager.py
@@ -1,44 +1,115 @@
 # =====================================
-# Forex_mustfa_ai V2
+# Forex_mustfa_ai V3
 # RISK MANAGER
 # =====================================
 
+
 def calculate_trade(price, signal, atr):
 
-    if signal == "BUY":
 
-        entry = price
-        stop_loss = price - (atr * 1.5)
+    # لا توجد صفقة
 
-        tp1 = price + (atr * 1.5)
-        tp2 = price + (atr * 2)
-        tp3 = price + (atr * 3)
-
-    elif signal == "SELL":
-
-        entry = price
-        stop_loss = price + (atr * 1.5)
-
-        tp1 = price - (atr * 1.5)
-        tp2 = price - (atr * 2)
-        tp3 = price - (atr * 3)
-
-    else:
+    if signal not in ["BUY", "SELL"]:
 
         return {
+
             "entry": "-",
+
             "stop_loss": "-",
+
             "tp1": "-",
+
             "tp2": "-",
+
             "tp3": "-",
-            "expected_points": "-"
+
+            "expected_points": "-",
+
+            "risk_reward": "-",
+
+            "atr": round(atr,5)
+
         }
 
 
-    expected_points = abs(tp2 - entry) * 10000
+
+    # =========================
+    # ATR SETTINGS
+    # =========================
+
+    sl_distance = atr * 1.5
+
+    tp1_distance = atr * 1.5
+
+    tp2_distance = atr * 2.5
+
+    tp3_distance = atr * 3.5
+
+
+
+    # =========================
+    # BUY
+    # =========================
+
+    if signal == "BUY":
+
+
+        entry = price
+
+        stop_loss = price - sl_distance
+
+        tp1 = price + tp1_distance
+
+        tp2 = price + tp2_distance
+
+        tp3 = price + tp3_distance
+
+
+
+    # =========================
+    # SELL
+    # =========================
+
+    else:
+
+
+        entry = price
+
+        stop_loss = price + sl_distance
+
+        tp1 = price - tp1_distance
+
+        tp2 = price - tp2_distance
+
+        tp3 = price - tp3_distance
+
+
+
+    # =========================
+    # CALCULATIONS
+    # =========================
+
+
+    risk = abs(entry - stop_loss)
+
+    reward = abs(tp2 - entry)
+
+
+    risk_reward = round(
+        reward / risk,
+        2
+    )
+
+
+    expected_points = round(
+        reward * 10000,
+        1
+    )
+
 
 
     return {
+
 
         "entry": round(entry,5),
 
@@ -50,6 +121,10 @@ def calculate_trade(price, signal, atr):
 
         "tp3": round(tp3,5),
 
-        "expected_points": round(expected_points,1)
+        "expected_points": expected_points,
+
+        "risk_reward": risk_reward,
+
+        "atr": round(atr,5)
 
     }
