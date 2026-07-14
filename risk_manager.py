d89367129b948016119f69cf8648d74bf4ce2686
manarr9801-ship-it <manarr9801@gmail.com>
2026-07-14 14:47:25 +0300

    fix risk_manager.py

--- risk_manager.py
+++ risk_manager.py
@@ -3,37 +3,35 @@
 # RISK MANAGER
 # =====================================
 
-def calculate_trade_levels(signal_data):
-
-    signal = signal_data["signal"]
-    entry = signal_data["price"]
-
-    stop_distance = 0.0020
-
+def calculate_trade(price, signal, atr):
 
     if signal == "BUY":
 
-        stop_loss = entry - stop_distance
+        entry = price
+        stop_loss = price - (atr * 1.5)
 
-        tp1 = entry + stop_distance
-        tp2 = entry + (stop_distance * 2)
-        tp3 = entry + (stop_distance * 3)
-
+        tp1 = price + (atr * 1.5)
+        tp2 = price + (atr * 2)
+        tp3 = price + (atr * 3)
 
     elif signal == "SELL":
 
-        stop_loss = entry + stop_distance
+        entry = price
+        stop_loss = price + (atr * 1.5)
 
-        tp1 = entry - stop_distance
-        tp2 = entry - (stop_distance * 2)
-        tp3 = entry - (stop_distance * 3)
-
+        tp1 = price - (atr * 1.5)
+        tp2 = price - (atr * 2)
+        tp3 = price - (atr * 3)
 
     else:
 
         return {
-            "signal": "WAIT",
-            "entry": entry
+            "entry": "-",
+            "stop_loss": "-",
+            "tp1": "-",
+            "tp2": "-",
+            "tp3": "-",
+            "expected_points": "-"
         }
 
 
@@ -42,18 +40,16 @@ def calculate_trade_levels(signal_data):
 
     return {
 
-        "signal": signal,
+        "entry": round(entry,5),
 
-        "entry": round(entry, 5),
+        "stop_loss": round(stop_loss,5),
 
-        "stop_loss": round(stop_loss, 5),
+        "tp1": round(tp1,5),
 
-        "tp1": round(tp1, 5),
+        "tp2": round(tp2,5),
 
-        "tp2": round(tp2, 5),
+        "tp3": round(tp3,5),
 
-        "tp3": round(tp3, 5),
-
-        "expected_points": round(expected_points, 1)
+        "expected_points": round(expected_points,1)
 
     }
