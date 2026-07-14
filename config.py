b5299401e9503412897198676d4cef05527465fb
manarr9801-ship-it <manarr9801@gmail.com>
2026-07-14 14:40:15 +0300

    Fix config.py Telegram settings

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 # CONFIGURATION
 # =====================================
 
+
 SYMBOL = "EUR/USD"
 
 INTERVAL = "1h"
@@ -12,9 +13,11 @@ OUTPUT_SIZE = 200
 # Twelve Data
 API_KEY = "8ddb1cdc9051436b8fc9bfd775b913fc"
 
+
 # Telegram
-TOKEN = "8229091266:AAHzWGVuzi_F4KCiJ1naL7_knKPBr21E93k"
-CHAT_ID = " 729720320"
+BOT_TOKEN = "8229091266:AAHzWGVuzi_F4KCiJ1naL7_knKPBr21E93k"
+CHAT_ID = "729720320"
+
 
 # Risk Management
 RISK_PERCENT = 1
